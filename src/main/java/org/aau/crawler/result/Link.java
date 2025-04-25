784b4beba690b280a34f142ce96b1105175a613b
EduardKlaus <klaus.eduard96@gmail.com>
2025-04-25 14:01:42 +0200

    getDepth and hashCode method added to fix Feature Envy-Smell

--- src/main/java/org/aau/crawler/result/Link.java
+++ src/main/java/org/aau/crawler/result/Link.java
@@ -16,11 +16,20 @@ public abstract class Link {
         return url;
     }
 
+    public int getDepth() {
+        return depth;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Link link = (Link) o;
         return Objects.equals(url, link.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(url);
     }
 }
