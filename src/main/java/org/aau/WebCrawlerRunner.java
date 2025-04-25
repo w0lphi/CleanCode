98233804ab06c21e1a2008b6da671cb96260dd00
EduardKlaus <klaus.eduard96@gmail.com>
2025-04-25 20:58:01 +0200

    Entfernung der Parsing, Analyse und Ausgabe aus der Klasse (Single Choice)

--- src/main/java/org/aau/WebCrawlerRunner.java
+++ src/main/java/org/aau/WebCrawlerRunner.java
@@ -1,2 +1,20 @@
-package org.aau;public class WebCrawlerRunner {
+package org.aau;
+
+import org.aau.crawler.*;
+import org.aau.writer.*;
+import org.aau.crawler.client.WebCrawlerClient;
+
+import java.nio.file.Path;
+import java.time.OffsetDateTime;
+
+    public class WebCrawlerRunner {
+        public Path run(String startUrl, int maxDepth, String outputDir) throws Exception {
+            var client = new WebCrawlerClient();
+            var analyzer = new PageAnalyzer(new HtmlParser());
+            var crawler = new WebCrawler(startUrl, maxDepth, client, analyzer);
+            crawler.start();
+
+            var writer = new MarkdownWriter(outputDir, new MarkdownFormatter());
+            return writer.writeResultsToFile(crawler.getCrawledLinks(), OffsetDateTime.now());
+        }
 }
