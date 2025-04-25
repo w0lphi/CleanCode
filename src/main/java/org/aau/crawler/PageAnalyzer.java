84a17a0676b35712dbdbe74c690d53c3ef54a803
EduardKlaus <klaus.eduard96@gmail.com>
2025-04-25 20:56:16 +0200

    Analyselogik wird weiter aufgeteilt (SingleChoice)

--- src/main/java/org/aau/crawler/PageAnalyzer.java
+++ src/main/java/org/aau/crawler/PageAnalyzer.java
@@ -1,2 +1,23 @@
-package org.aau.crawler;public class PageAnalyzer {
+package org.aau.crawler;
+
+import org.aau.crawler.result.WorkingLink;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.Set;
+
+public class PageAnalyzer {
+
+    private final HtmlParser parser;
+
+    public PageAnalyzer(HtmlParser parser) {
+        this.parser = parser;
+    }
+
+    public WorkingLink analyze(String url, int depth, String html) {
+        Document doc = Jsoup.parse(html, url);
+        Set<String> links = parser.extractLinks(doc);
+        Set<String> headings = parser.extractHeadings(doc);
+        return new WorkingLink(url, depth, headings, links);
+    }
 }
