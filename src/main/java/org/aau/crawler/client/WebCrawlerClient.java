c83fe03b54dd881edc76673fddc28933dd508f82
Philipp Wolfger <philipp.wolfger@alturos.com>
2025-06-09 14:14:09 +0200

    Removed remaining third-party library code

--- src/main/java/org/aau/crawler/client/WebCrawlerClient.java
+++ src/main/java/org/aau/crawler/client/WebCrawlerClient.java
@@ -1,10 +1,7 @@
 package org.aau.crawler.client;
 
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.UnhandledAlertException;
-
 public interface WebCrawlerClient extends AutoCloseable {
     boolean isPageAvailable(String url);
 
-    String getPageContent(String url) throws TimeoutException, UnhandledAlertException;
+    String getPageContent(String url) throws RuntimeException;
 }
