aa00985a9315f0f5f28c633853a29f1bcaea85b7
Philipp Wolfger <philipp.wolfger@alturos.com>
2025-06-09 12:21:42 +0200

    Restored MarkdownWriterIntegrationTest

--- src/test/java/org/aau/writer/MarkdownWriterIntegrationTest.java
+++ src/test/java/org/aau/writer/MarkdownWriterIntegrationTest.java
@@ -27,22 +27,39 @@ public class MarkdownWriterIntegrationTest {
 
     @Test
     void testWriteResultsToFile() throws IOException {
+        Path expectedPath = Path.of(TEST_FILE_PATH).toAbsolutePath();
         MarkdownWriter writer = new MarkdownWriter(TEST_FILE_PATH);
         Set<String> headings = new LinkedHashSet<>(List.of("Heading1", "Heading2", "Heading3"));
         Link workingLink = new WorkingLink("https://www.working.com", 1, headings, Set.of("Link3"));
         Link brokenLink = new BrokenLink("https://www.broken.com", 5);
         Set<Link> links = new LinkedHashSet<>(List.of(workingLink, brokenLink));
-
         OffsetDateTime timestamp = OffsetDateTime.now();
-        Path expectedPath = Path.of(TEST_FILE_PATH).toAbsolutePath()
-                .resolve("report-" + timestamp.format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss")) + ".md");
+        String expectedContent = """
+                # Crawl Results
+                
+                Timestamp: %s
+                
+                ## https://www.working.com
+                Depth: 1
+                ### Headings
+                Heading1\s\s
+                Heading2\s\s
+                Heading3
+                
+                ## https://www.broken.com (broken)
+                Depth: 5
+                
+                """.formatted(timestamp.format(DATE_TIME_FORMATTER));
 
         Path filePath = writer.writeResultsToFile(links, timestamp);
-
-        assertEquals(expectedPath, filePath.toAbsolutePath());
+        assertEquals(expectedPath + "/report-" + timestamp.format(DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss")) + ".md", filePath.toAbsolutePath().toString());
         assertTrue(Files.exists(filePath));
 
-        Files.deleteIfExists(filePath);
+        StringBuilder builder = new StringBuilder();
+        for (String line : Files.readAllLines(filePath)) {
+            builder.append(line).append(System.lineSeparator());
+        }
+        assertEquals(expectedContent, builder.toString());
     }
 
     @AfterEach
